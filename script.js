e6a4bc1590ef4c8906154a320352602f6602f263
Per Ostojin <perostojin@outlook.com>
2026-08-31 21:34:09 +0200

    Remove unused character count warning

--- script.js
+++ script.js
@@ -4,9 +4,4 @@ const characterCount = document.getElementById('character-count');
 messageInput.addEventListener('input', () => {
   const currentLength = messageInput.value.length;
   characterCount.textContent = `${currentLength} av 500 tecken`;
-  if (currentLength > 500) {
-    characterCount.style.color = 'red';
-  } else {
-    characterCount.style.color = '';
-  }
-});q
+});
